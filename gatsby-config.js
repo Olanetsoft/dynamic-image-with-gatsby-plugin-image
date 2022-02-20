49bdf2051f494ef9df9a31e71557257f2c988488
Idris Olubisi <heedris2olubisi@gmail.com>
2022-02-20 17:46:52 +0100

    File Upload

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,24 @@
 module.exports = {
   siteMetadata: {
-      title: `gatsby-dynamic-image`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `gatsby-dynamic-image`,
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [{
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }]
+  plugins: [
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`, // Needed for dynamic images
+  ],
 };
